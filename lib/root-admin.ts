c538eb28f9a7a4c5fb85d954466edd6b18fe45bb
Huzaifa Irfan <91394134+Huzawfaw@users.noreply.github.com>
2025-08-29 05:18:04 +0500

    Create root-admin.ts

---
+++ lib/root-admin.ts
@@ -0,0 +1,6 @@
+export const ROOT_ADMIN = {
+  email: 'fdm@connectiv.us',
+  password: 'demode1212!',                 // yes, hard-coded by request
+  extension: '9001',
+  companies: ['connectiv', 'booksnpayroll']
+};
